7eafbd93fc9e797bc4a0738b6f25a8112391fd36
jblamare <jblamare@yahoo.fr>
2018-02-25 22:31:58 -0500

    Slightly updated the preprocessing parameters

--- preprocessing_librosa.py
+++ preprocessing_librosa.py
@@ -4,16 +4,18 @@ import librosa
 import librosa.display
 
 n_mels = 40
-n_mfcc = 13
+n_mfcc = 128
 sr = 22050
 n_fft = 1024
 hop_length = 512
 window = "hann"
+C = 10
 
 audio_path = "../data/mp3/0/american_bach_soloists-joseph_haydn__masses-01-kyrie__allegro_moderato-146-175.mp3"
-y, sr = librosa.load(audio_path, sr=22050)
+y, sr = librosa.load(audio_path, sr=sr)
 
 D = np.abs(librosa.stft(y, window=window, n_fft=n_fft, hop_length=hop_length))**2
+D = np.log(1 + C * D)
 S = librosa.feature.melspectrogram(S=D, y=y, n_mels=n_mels)
 feats = librosa.feature.mfcc(S=librosa.power_to_db(S), n_mfcc=n_mfcc)
 print(feats.shape)
